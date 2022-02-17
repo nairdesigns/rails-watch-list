f75a89007621be8fd423a7e338552453c121837e
nairdesigns <nairbdg@gmail.com>
2022-02-17 15:01:39 +0900

    uniqueness for bookmakr finally solveed

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,6 +1,6 @@
 class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
-  validates :movie, presence: true
-  validates :list, presence: true
+  validates :movie_id, presence: true, uniqueness: { scope: :list_id }
+  # validates :list, presence: true, uniqueness: true
 end
