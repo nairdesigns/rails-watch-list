a72018b1352ccae39c6cd020bf69e56ff7f722da
nairdesigns <nairbdg@gmail.com>
2022-02-17 16:23:53 +0900

    movies showing '

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,26 @@
 class MoviesController < ApplicationController
+  def index
+    @movies = Movie.all
+  end
+
+  # def show
+  #   @movie = Movie.find(params[:id])
+  # end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+    if @movie.save
+      redirect_to movie_path(@movie)
+    else
+      render :new
+    end
+  end
+
+  def movie_params
+    params.require(:movie).permit(:title, :overview, :rating)
+  end
 end
