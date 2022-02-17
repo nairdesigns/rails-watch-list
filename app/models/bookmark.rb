448705070d90ca83e7dc027d6262af4135a93f0a
nairdesigns <nairbdg@gmail.com>
2022-02-17 15:40:36 +0900

    seed generator done

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -3,4 +3,5 @@ class Bookmark < ApplicationRecord
   belongs_to :list
   validates :movie_id, presence: true, uniqueness: { scope: :list_id }
   # validates :list, presence: true, uniqueness: true
+  validates :comment, length: { minimum: 10 }
 end
