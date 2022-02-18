0fda66bc8cfc041c091065f89240860d1909371d
nairdesigns <nairbdg@gmail.com>
2022-02-18 12:51:33 +0900

    database working

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,7 +1,12 @@
 class Bookmark < ApplicationRecord
+  # belongs_to :movie
+  # belongs_to :list
+  # validates :movie_id, presence: true, uniqueness: { scope: :list_id }
+  # # validates :list, presence: true, uniqueness: true
+  # validates :comment, length: { minimum: 10 }
   belongs_to :movie
   belongs_to :list
-  validates :movie_id, presence: true, uniqueness: { scope: :list_id }
-  # validates :list, presence: true, uniqueness: true
-  validates :comment, length: { minimum: 10 }
+
+  validates :comment, length: { minimum: 6 }
+  validates :movie_id, uniqueness: { scope: :list_id, message: 'is already in the list' }
 end
